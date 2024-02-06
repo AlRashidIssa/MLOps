0d9266d97d135ff2f184d6b0a6bc01f162cb96cc
AlRashidissa <rashidissa2001@icloud.com>
2024-02-06 22:28:35 +0300

    Reload data

--- Customer Satisfaction project/run_pipeline.py
+++ Customer Satisfaction project/run_pipeline.py
@@ -3,4 +3,4 @@ from pipelines.training_pipeline import train_pipeline
 if __name__ == "__main__":
     # run the pipeline
     train_pipeline(data_path=
-                   "/home/alrashidissa/Desktop/MLOps/Customer Satisfaction project/data/olist_customers_dataset.csv")
+                   "/home/alrashidi/Desktop/MLOps/Customer Satisfaction project/data/olist_customers_dataset.csv")
